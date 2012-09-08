949439de999c6644fa87b2901ce0fb690fcf1e8d
Dirk Klemmt <dirch@quantentunnel.de>
2012-09-08 17:38:30 +0200

    initial import

---
+++ system/modules/jquery_easing/config/autoload.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2012 Leo Feyer
+ * 
+ * @package   jquery_easing 
+ * @author    Dirk Klemmt 
+ * @license   GPL 
+ * @copyright Dirk Klemmt 2012 
+ */
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'j_easing'	=> 'system/modules/jquery_easing/templates'
+));
---
+++ system/modules/jquery_easing/languages/de/modules.php
@@ -0,0 +1,24 @@
+<?php 
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2012 Leo Feyer
+ * 
+ * @package   jquery_easing 
+ * @author    Dirk Klemmt 
+ * @license   GPL 
+ * @copyright Dirk Klemmt 2012 
+ */
+
+
+/**
+ * Back end modules
+ */
+$GLOBALS['TL_LANG']['MOD']['jquery_easing'] = array('jQuery easing plugin', '');
+
+
+/**
+ * Front end modules
+ */
+$GLOBALS['TL_LANG']['FMD']['jquery_easing'] = array('jQuery easing plugin', '');
