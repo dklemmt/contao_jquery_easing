e61621af01df9e5997ceeab46e7e0c496d90a82b
dklemmt <dirch@quantentunnel.de>
2013-07-03 00:18:01 +0200

    changed script to minified version

--- system/modules/jquery_easing/languages/de/modules.php
+++ system/modules/jquery_easing/languages/de/modules.php
@@ -3,22 +3,16 @@
 /**
  * Contao Open Source CMS
  * 
- * Copyright (C) 2005-2012 Leo Feyer
+ * Copyright (C) 2005-2013 Leo Feyer
  * 
- * @package   jquery_easing 
- * @author    Dirk Klemmt 
- * @license   GPL 
- * @copyright Dirk Klemmt 2012 
+ * @package   jquery_easing
+ * @author    Dirk Klemmt
+ * @license   GPL
+ * @copyright Dirk Klemmt 2012-2013
  */
-
+ 
 
 /**
  * Back end modules
  */
-$GLOBALS['TL_LANG']['MOD']['jquery_easing'] = array('jQuery easing plugin', '');
-
-
-/**
- * Front end modules
- */
-$GLOBALS['TL_LANG']['FMD']['jquery_easing'] = array('jQuery easing plugin', '');
+$GLOBALS['TL_LANG']['MOD']['jquery_easing'][0] = 'jQuery Easing Plugin';
